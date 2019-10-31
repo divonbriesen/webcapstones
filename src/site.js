d4979e6817b77be262d46cff88f695cbd76b7d15
Dubman <keithdublin@gmail.com>
2019-10-31 13:46:22 -0400

    Serving HTML page.

--- src/site.js
+++ src/site.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var fileSystem = require('fs');
 
-http.createServer(function (req, res) {
-    res.write('Hello World!');
-    res.end();
+var indexPageBuffer = fileSystem.readFileSync('index.html');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(indexPageBuffer);
 }).listen(3000);
 
 console.log("Website is running . . .");
