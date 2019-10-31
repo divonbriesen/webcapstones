fdeffb22a705d3e404993054edf234d4887063e9
Dubman <keithdublin@gmail.com>
2019-10-31 14:34:00 -0400

    Serving static pages.

--- src/site.js
+++ src/site.js
@@ -1,11 +1,13 @@
 var http = require('http');
 var fileSystem = require('fs');
+var express = require('express');
+
+var app = express();
 
 var indexPageBuffer = fileSystem.readFileSync('index.html');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end(indexPageBuffer);
-}).listen(3000);
+app.use(express.static('.'));
+
+var server = app.listen(3000);
 
 console.log("Website is running . . .");
