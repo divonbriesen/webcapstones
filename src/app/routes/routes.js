197d7042fdca44a5fe9c9b3f2902424d9b8ce1b4
Dubman <keithdublin@gmail.com>
2019-10-31 21:34:02 -0400

    Get Entity by ID API working.

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,21 +1,37 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
-    
+
     app.post('/entity', (request, response) => {
+
             const entity = { Name: request.body.Name, Description: request.body.Description };
-            console.log(entity);
 
-            // db.collection('entities').insert(entity, (error, result) => {
-            //     if (error) {
-            //         response.send({
-            //             'error': 'An error has occurred'
-            //         });
-            //     }
-            //     else {
-            //         response.send(result.ops[0]);
-            //     }
-            // });
-
-            response.send(entity);
+            db.collection('entities').insert(entity, (error, result) => {
+                if (error) {
+                    response.send({
+                        'error': 'An error has occurred'
+                    });
+                }
+                else {
+                    response.send(result.ops[0]);
+                }
+            });
         }
     );
+
+    app.get('/entity/:id', (request, response) => {
+
+        const id = request.params.id;
+        const details = { '_id': new ObjectID(id) };
+
+        const collection = db.collection('entities');
+
+        collection.findOne(details, (error, item) => {
+            if (error) {
+                response.send( {'error': 'An error has occurred'});
+            }
+
+            response.send(item);
+        })
+    });
 };
